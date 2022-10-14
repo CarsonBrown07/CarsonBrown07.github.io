10f002a871349e5319d2304b77984863b5624435
CarsonBrown07 <cbb0195153@lpssonline.com>
2022-10-14 14:26:16 +0000

    update

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -27,7 +27,7 @@
     // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
 
     // example:
-    createCannon("top", 450);
+    createCannon("top", 550);
     createCannon("right", 720); // a cannon on the right side of the queen located at y = 100
     createCannon("top", 450); // a cannon on the top of the screen, located at x = 450
     createCannon("left", 300, 1000); // a cannon on the left side of the screen, located at y = 300, with a 1 second delay (1000ms)
